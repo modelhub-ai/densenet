ecc4ac6ea130502ccce5aecd43003a99e8613121
Ahmed Hosny <i.ahmedhosny@gmail.com>
2019-03-21 11:03:20 -0400

    grayscale image cleanup

--- contrib_src/processing.py
+++ contrib_src/processing.py
@@ -9,19 +9,21 @@ class ImageProcessor(ImageProcessorBase):
 
     def _preprocessBeforeConversionToNumpy(self, image):
         if isinstance(image, PIL.Image.Image):
-            image = cv2.resize(np.array(image).astype(np.float32), (224, 224)).astype(np.float32)
-            image = image[:, :, ::-1].copy()
+            # convert to numpy
+            image = np.array(image).astype(np.float32)
+            image = cv2.resize(image, (224, 224)).astype(np.float32)
+            # Convert RGB to BGR
+            if len(image.shape) > 2:
+                image = image[:, :, ::-1].copy()
         else:
             raise IOError("Image Type not supported for preprocessing.")
         return image
 
     def _preprocessAfterConversionToNumpy(self, npArr):
-        if npArr.shape[0] > 3:
-            npArr = npArr[0:3,:,:]
-        elif npArr.shape[0] < 3:
-            npArr = npArr[[0],:,:]
-            npArr = np.concatenate((npArr, npArr[[0],:,:]), axis = 1)
-            npArr = np.concatenate((npArr, npArr[[0],:,:]), axis = 1)
+        if len(npArr.shape) > 2:
+            npArr = npArr[:,:,0:3]
+        else:
+            npArr = np.stack((npArr,)*3, axis=-1)
         npArr[:,:,0] = (npArr[:,:,0] - 103.94) * 0.017
         npArr[:,:,1] = (npArr[:,:,1] - 116.78) * 0.017
         npArr[:,:,2] = (npArr[:,:,2] - 123.68) * 0.017
